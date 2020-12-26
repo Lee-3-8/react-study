b8a895dd6943d25e42c5b3b8d37d3a5dc010b80b
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-26 23:30:26 +0900

    component life cycle

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -13,6 +13,26 @@ class App extends React.Component{
   minus =() =>{
     this.setState(current => ({count : this.state.count -1 }));
   };  
+
+
+/*
+life cycle method!
+component 가 render를 호출하고 컴포넌트가 업데이트될때 호출되는게 있다.
+각각 메소드의 호출될때의 세부내용
+1. mounting 
+  - Constructor
+  - getDerivedStateFromProps() -> 잘안씀
+  - render!
+  - componentDidMount
+2. updating
+  - getDerivedStateFromProps
+  - shoul~
+  ~~~
+  - render
+  - componentDidUpdate
+3. unmounting -> 컴포넌트가 죽을때 (페이지가 바뀌는 등)
+*/
+
   render() { //리엑트는 자동적으로 render매소드를 시작함!!
     return (
       <div>
